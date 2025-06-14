022f31c367a36ae1b4fa0ac566ada910d7b0d55e
chadwin-uhuru <uhuruchadwin166@gmail.com>
2025-06-14 12:43:34 +0300

    Implement write_file, append_file, and read_file functions for File IO Lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,18 @@
+from pathlib import Path
+
 def write_file(file_name, file_content):
-    pass
+    file_path = Path(file_name).with_suffix('.txt')
+    with open(file_path, "w") as file:
+        file.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    file_path = Path(file_name).with_suffix('.txt')
+    with open(file_path, "a") as file:
+        file.write(append_content)
+
 
 def read_file(file_name):
-    pass
+    file_path = Path(file_name).with_suffix('.txt')
+    with open(file_path, "r") as file:
+        return file.read()
